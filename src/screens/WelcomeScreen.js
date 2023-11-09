2ca5af0b932fd5ba1f30530c488ebdb529be6e2b
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2023-11-09 20:33:54 +0530

    commit round spring

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,18 +1,48 @@
 import {View, Text, StatusBar, Image} from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import Animated from 'react-native-reanimated';
+import {useSharedValue} from 'react-native-reanimated';
 
 export default function WelcomeScreen() {
+  const ring1padding = userSharedValue(0);   
+  const ring2padding = userSharedValue(0);
+
+  useEffect(()=>{
+    setTimeout(()=>
+      ring1padding.value = withSpring(ring1padding.value+hp(5),100));
+      setTimeout(()=>
+      ring2padding.value = withSpring(ring2padding.value+hp(5.5),100));
+  },[])
   return (
     <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
       <StatusBar style="light" />
+      
       {/* logo image with rings */}
-      <View>
-        <View>
+      <Animated.View className="bg-white/20 rounded-full " style={{padding: ring2padding}}>
+        <Animated.View className="bg-white/20 rounded-full " style={{padding: ring1padding}}>
           <Image
             source={require('../../assets/images/welcome.png')}
-            style={{width: 200, height: 200}}
+            style={{width: hp(20), height: hp(20)}}
           />
-        </View>
+        </Animated.View>
+      </Animated.View>
+
+      {/* title and punchline */}
+      <View className="flex items-center space-y-2">
+        <Text
+          style={{fontSize: hp(7)}}
+          className="font-bold text-white tracking-widest">
+          Foody
+        </Text>
+        <Text
+          style={{fontSize: hp(2)}}
+          className="font-medium text-white tracking-widest">
+          Food is always right
+        </Text>
       </View>
     </View>
   );
